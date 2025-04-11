20ff7def8359776222e6c85aabd0009958b569a4
Leonardo da Silveira <leodsilveira@hotmail.com>
2025-04-10 21:42:23 -0300

    Dicionário de Recursos - Versão Final

--- DicionarioRecursos/DicionarioRecursos/obj/Debug/net9.0-ios/iossimulator-x64/DicionarioRecursos.AssemblyInfo.cs
+++ DicionarioRecursos/DicionarioRecursos/obj/Debug/net9.0-ios/iossimulator-x64/DicionarioRecursos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DicionarioRecursos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fc7cbb9b6fac132d662ecb9e5fb77d1cf646f360")]
 [assembly: System.Reflection.AssemblyProductAttribute("DicionarioRecursos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DicionarioRecursos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
